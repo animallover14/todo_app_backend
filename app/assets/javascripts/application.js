4d9dbfe5dcd9078c2aa901aa3190e066f2b37b78
Helen Spence <horselover7485@icloud.com>
2018-07-11 15:30:39 +0000

    added conditions to todos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,25 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+    console.log("helloworld");
+    
+    $("#todobutton").click(function(){
+        console.log("Button works")
+        var numberofpomadaros=$("#pomadarosinput").val()
+        var nameoftodo=$("#todoinpput").val()
+        console.log(nameoftodo)
+        $("ul").append("<li><input type='checkbox' />" 
+        + nameoftodo + "<span class='time'>" 
+        + numberofpomadaros 
+        + " pomadaros </span></li>")
+    })
+$("input[type='checkbox']").click(function(){
+    $(this).closest("li").toggleClass("completed")
+    $(this).closest("span").toggleClass("completed-time")
+}
+)
+
+})
+
